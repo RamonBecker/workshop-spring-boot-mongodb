ede12f2ca37cd1b7a0e62329caf8316d325adedc
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2021-10-23 22:28:50 -0300

    Deleção de usuário com DELETE

--- worshopmongo/src/main/java/com/workshop/course/services/UserService.java
+++ worshopmongo/src/main/java/com/workshop/course/services/UserService.java
@@ -30,6 +30,10 @@ public class UserService {
 		return repo.insert(user);
 	}
 	
+	public void delete(String id) { 
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO userDTO) {
 		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
