1909a61d55005d730f75e9a7ad22c31bde8be8a2
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2021-10-24 10:40:02 -0300

    Referenciando os posts do usuário

--- worshopmongo/src/main/java/com/workshop/course/config/Instantiation.java
+++ worshopmongo/src/main/java/com/workshop/course/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.workshop.course.config;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -35,16 +36,17 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
-		
+
 		Post post1 = new Post(null, sdf.parse("21/10/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
 				new UserDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/10/2021"), "Bom dia", "Acordei feliz hoje!", new UserDTO(maria));
 
-
 		postRepository.saveAll(Arrays.asList(post1, post2));
+
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
