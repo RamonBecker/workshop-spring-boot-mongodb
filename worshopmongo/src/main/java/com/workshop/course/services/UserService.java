bcbda1cc7387b1483d13ee8cdb3969b22435450d
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2021-10-23 22:14:49 -0300

    Obtendo um usuário por id. Criando exceptions nas camada de resouce:  StandardError e ResourceExceptionHandler

--- worshopmongo/src/main/java/com/workshop/course/services/UserService.java
+++ worshopmongo/src/main/java/com/workshop/course/services/UserService.java
@@ -1,20 +1,27 @@
 package com.workshop.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.course.domain.User;
 import com.workshop.course.repository.UserRepository;
+import com.workshop.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("User not found"));
 	}
 }
